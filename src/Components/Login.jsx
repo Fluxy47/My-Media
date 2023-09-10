03cc962ccd37e35ac56dd4c7b3b452114ce1936e
Fluxy <ali.scorpio47@gmail.com>
2023-09-11 00:11:36 +0500

    Updated Login component and index.css

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -44,7 +44,7 @@ function Login() {
           controls={false}
           muted
           autoPlay
-          className="w-full h-full object-cover"
+          className="w-full h-full  object-cover"
         />
 
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
